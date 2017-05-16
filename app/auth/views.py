73f090ff4c593134c694efc9c113060938d20900
Joanna Marie <joannaompoc@gmail.com>
2017-05-16 12:17:59 +0800

    added settings for admin

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, Blueprint, request, flash, url_for, session, jsonify
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user, AnonymousUserMixin
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 from model import User, Role, Anonymous, Photos, Connection
 from forms import LoginForm, RegisterForm, EditForm, SearchForm, PasswordSettingsForm, UsernameSettingsForm
 from app import db, app
