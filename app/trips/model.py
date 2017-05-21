8e07e1194932d7b228dbe1fdb80bbde1bcedf749
Shealtiel Canon <funwifgaming@gmail.com>
2017-05-21 13:38:38 +0800

    seeding function for city and country model

--- app/trips/model.py
+++ app/trips/model.py
@@ -70,22 +70,20 @@ class City(db.Model):
     __tablename__ = "cities"
     cityID = db.Column(db.Integer, primary_key=True)
     cityName = db.Column(db.String(80))
-    cityCode = db.Column(db.Integer)
-    countryID = db.Column(db.Integer, db.ForeignKey('countries.countryID'), nullable=False)
+    cityCode = db.Column(db.String(80))
 
-    def __init__(self, cityName, cityCode, countryID):
+    def __init__(self, cityName, cityCode):
         self.cityName = cityName
-        self.cityName = cityCode
-        self.countryID = countryID
+        self.cityCode = cityCode
 
     def __repr__(self):
-        return '<cityName {}>'.format(self.cityCode)
+        return '<cityName {}>'.format(self.cityName)
 
 class Country(db.Model):
     __tablename__ = "countries"
     countryID = db.Column(db.Integer, primary_key=True)
     countryName = db.Column(db.String(80))
-    countryCode = db.Column(db.Integer)
+    countryCode = db.Column(db.String(80))
 
     def __init__(self, countryName, countryCode):
         self.countryName = countryName
--- config.py
+++ config.py
@@ -6,4 +6,6 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 
 dbstring = 'postgresql://postgres:databaseadmin@127.0.0.1:5432/travelplannerdb'
+seedcountrystring = 'postgresql://postgres:databaseadmin@127.0.0.1:5432/travelplannerdb::countries'
+seedcitystring = 'postgresql://postgres:databaseadmin@127.0.0.1:5432/travelplannerdb::cities'
 
