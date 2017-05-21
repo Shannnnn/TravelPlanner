67eb2e4c46302bd3fae787b7cb8dd97f2ac1bcfd
Shealtiel Canon <funwifgaming@gmail.com>
2017-05-21 14:34:26 +0800

    cleaning code

--- config.py
+++ config.py
@@ -7,8 +7,3 @@ app = Flask(__name__)
 dbstring = 'postgresql://postgres:databaseadmin@127.0.0.1:5432/travelplannerdb'
 seedcountrystring = 'postgresql://postgres:databaseadmin@127.0.0.1:5432/travelplannerdb::countries'
 seedcitystring = 'postgresql://postgres:databaseadmin@127.0.0.1:5432/travelplannerdb::cities'
-<<<<<<< HEAD
-=======
-
-
->>>>>>> Trips
