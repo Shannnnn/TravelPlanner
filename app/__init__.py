c4a2e7c1da4c1bba09e5d2bd5754f7c6df33da0d
Shealtiel Canon <funwifgaming@gmail.com>
2017-05-21 14:29:53 +0800

    cleaning codes

--- app/__init__.py
+++ app/__init__.py
@@ -5,16 +5,6 @@ from flask_mail import Mail
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from config import dbstring
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
-
->>>>>>> Trips
-=======
-
-
->>>>>>> Trips
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
@@ -30,13 +20,6 @@ mail = Mail(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = dbstring
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tkakffdbqitxpz:d06692592a1ccafca6ec426d1c8f13a5339cfb7792c6f757dc8dc6a3e0c8379d@ec2-23-21-235-142.compute-1.amazonaws.com:5432/df163mhfv9kda9'
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> Trips
-=======
->>>>>>> Trips
 
 migrate = Migrate(app, db)
 manager = Manager(app)
