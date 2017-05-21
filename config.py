11b45925d2fde6085755a9a0bdfaebd6deb6f321
Shealtiel Canon <funwifgaming@gmail.com>
2017-05-21 14:22:08 +0800

    cleaning repository

--- config.py
+++ config.py
@@ -2,20 +2,10 @@ from flask import Flask
 import os
 
 _basedir = os.path.abspath(os.path.dirname(__file__))
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-dbstring = 'postgresql://postgres:imawesome@127.0.0.1:5432/travelplannerdb'
-=======
-=======
->>>>>>> Trips
 app = Flask(__name__)
 
 dbstring = 'postgresql://postgres:databaseadmin@127.0.0.1:5432/travelplannerdb'
 seedcountrystring = 'postgresql://postgres:databaseadmin@127.0.0.1:5432/travelplannerdb::countries'
 seedcitystring = 'postgresql://postgres:databaseadmin@127.0.0.1:5432/travelplannerdb::cities'
 
-<<<<<<< HEAD
->>>>>>> Trips
-=======
->>>>>>> Trips
+
