f439958f318a5bdb364e4ccf90962b541638cdca
Shealtiel Canon <funwifgaming@gmail.com>
2017-05-24 21:11:58 +0800

    added csv file for admin user

--- seeding/seed.py
+++ seeding/seed.py
@@ -1,12 +1,15 @@
 from odo import odo, dshape
 import pandas as pd
-from config import seedcountrystring, seedcitystring
+from config import seedcountrystring, seedcitystring, adminstring
 
-ds = dshape("var * {countryName: string, countryCode: string, countryID: int64}")
-odo("anothersample.csv", pd.DataFrame, dshape=ds)
-odo("anothersample.csv", seedcountrystring)
+# ds = dshape("var * {countryName: string, countryCode: string, countryID: int64}")
+# odo("anothersample.csv", pd.DataFrame, dshape=ds)
+# odo("anothersample.csv", seedcountrystring)
+#
+# ds = dshape("var * {cityID: int64, cityName: string, cityCode: string, countryName: string}")
+# odo("world-cities.csv", pd.DataFrame, dshape=ds)
+# odo("world-cities.csv", seedcitystring)
 
-ds = dshape("var * {cityID: int64, cityName: string, cityCode: string, countryName: string}")
-odo("world-cities.csv", pd.DataFrame, dshape=ds)
-odo("world-cities.csv", seedcitystring)
-
+ds = dshape("var * {username: string, email: string, password: string, role_id: int64}")
+odo("adminuser.csv", pd.DataFrame, dshap=ds)
+odo("adminuser.csv", adminstring)
