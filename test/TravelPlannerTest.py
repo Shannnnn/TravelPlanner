fc119c29104da64c99b1f6f6bacd55b8eede9158
Dexter Esin <sdexterous@yahoo.com>
2017-05-26 08:51:54 +0800

    test skeleton

--- test/TravelPlannerTest.py
+++ test/TravelPlannerTest.py
@@ -1,9 +1,16 @@
 import unittest
 
-
 class TestTravePlanner(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def setUp(self):
+        self.db_fb, travelplanner.app.config['DATABASE'] = tempfile.mkstemp()
+        travelplanner.app.config['TESTING'] = True
+        self.app = travelplanner.app.test_client()
+        with travelplanner.app.app_context():
+            travelplanner.init_db()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlike(travelplanner.app.config['DATABASE'])
 
 
 if __name__ == '__main__':
