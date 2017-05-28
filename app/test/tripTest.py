a1a6e4ea8ac044db98c80d025c58f5dac7561263
Shealtiel Canon <funwifgaming@gmail.com>
2017-05-28 11:31:09 +0800

    trips and itineraries unit testing

--- app/test/tripTest.py
+++ app/test/tripTest.py
@@ -1,33 +1,17 @@
-from app import app, db
+from app import app
+import os
 import unittest
+import tempfile
 
-
-class TestTravePlanner(unittest.TestCase):
+class TripTestCase(unittest.TestCase):
     def setUp(self):
+        self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
         app.config['TESTING'] = True
-        app.config['DEBUG'] = False
-        app.config['WTF_CSRF_ENABLED'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:databaseadmin@127.0.0.1:5432/testdb'
         self.app = app.test_client()
-        db.create_all()
 
     def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-
-    def testAddTrip(self):
-        response = self.addTrip('Trip', '1997/01/01', '1997/01/01', 'Iligan City', 'Philippines', 1, '1495638841.png', 0, 1, 0)
-        self.assertEqual(response.status_code, 200)
-
-    def testAddItinerary(self):
-        response = self.addItinerary('ItnOne', 'ItnDesc', 'ItnLoc', '1997/01/01', '10:00', 1, 1)
-        self.assertEqual(response.status_code, 200)
-
-    def testViewTrips(self):
-        response = self.app.get('/trips')
-        self.assertEqual(response.status_code, 200)
-
-
+        os.close(self.db_fd)
+        os.unlink(app.config['DATABASE'])
 
     def addTrip(self, tripName, tripDateFrom, tripDateTo, tripCity, tripCountry, userID, img_thumbnail, status, visibility, featuredTrip):
         return self.app.post(
@@ -42,7 +26,17 @@ class TestTravePlanner(unittest.TestCase):
             data = dict(itineraryName=itineraryName, itineraryDesc=itineraryDesc, itineraryLocation=itineraryLocation, itineraryDate=itineraryDate, itineraryTime=itineraryTime, tripID=tripID, locationTypeID=locationTypeID)
             )
 
+    def testAddTrip(self):
+        response = self.addTrip('Trip', '1997/01/01', '1997/01/01', 'Iligan City', 'Philippines', 1, '1495638841.png', 0, 1, 0)
+        self.assertEqual(response.status_code, 200)
 
+    def testAddItinerary(self):
+        response = self.addItinerary('ItnOne', 'ItnDesc', 'ItnLoc', '1997/01/01', '10:00', 1, 1)
+        self.assertEqual(response.status_code, 200)
+
+    def testViewTrips(self):
+        response = self.app.get('/trips')
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
