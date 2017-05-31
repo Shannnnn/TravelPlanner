4057a16a2e3df882b7851306ea35e871b6c61e9e
Earl Mark Estrada Barangot <earlmarkbarangot@gmail.com>
2017-05-31 00:35:06 -0700

    update and fixed some bugs

--- app/auth/views.py
+++ app/auth/views.py
@@ -1250,7 +1250,7 @@ def register():
     else:
         if form.validate_on_submit():
             user = User(username=request.form['username'], email=request.form['email'],
-                        password=request.form['password'], role_id=3)
+                        password=request.form['password'], role_id=1)
             db.session.add(user)
             db.session.commit()
 
